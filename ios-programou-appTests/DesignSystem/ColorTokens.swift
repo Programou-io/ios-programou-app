e24efd8a4384db73804e7b6b57b57143c52d387c
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-05-18 21:20:41 -0300

    test: assegurando hexadecimal para a cor preta dos tokens

--- ios-programou-appTests/DesignSystem/ColorTokens.swift
+++ ios-programou-appTests/DesignSystem/ColorTokens.swift
@@ -2,7 +2,11 @@ import XCTest
 @testable import programou
 
 final class ColorTokensTests: XCTestCase {
-    func test() {
+    func test_white_asHexDecimalVaue() {
         XCTAssertEqual(ColorTokens.white.hex, "#FFF")
+    }
+    
+    func test_black_asHexDecimalVaue() {
+        XCTAssertEqual(ColorTokens.black.hex, "#000")
     }
 }
