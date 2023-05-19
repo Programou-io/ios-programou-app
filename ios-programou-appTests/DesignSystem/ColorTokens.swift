c06a1c5f6dfdf5c254d69fdc35e38f018f509ae0
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-05-18 21:32:15 -0300

    test: assegurando tons de cinza

--- ios-programou-appTests/DesignSystem/ColorTokens.swift
+++ ios-programou-appTests/DesignSystem/ColorTokens.swift
@@ -9,4 +9,19 @@ final class ColorTokensTests: XCTestCase {
     func test_black_asHexDecimalVaue() {
         XCTAssertEqual(ColorTokens.black.hex, "#000")
     }
+    
+    func test_gray100_asHexDecimalVaue() {
+        XCTAssertEqual(ColorTokens.black.hex, "#000")
+    }
+    
+    func test_graies_withHEXValue() {
+        XCTAssertEqual(ColorTokens.gray100.hex, "#E1E1E6")
+        XCTAssertEqual(ColorTokens.gray200.hex, "#A9A9B2")
+        XCTAssertEqual(ColorTokens.gray400.hex, "#7C7C8A")
+        XCTAssertEqual(ColorTokens.gray500.hex, "#505059")
+        XCTAssertEqual(ColorTokens.gray600.hex, "#323238")
+        XCTAssertEqual(ColorTokens.gray700.hex, "#29292E")
+        XCTAssertEqual(ColorTokens.gray800.hex, "#202024")
+        XCTAssertEqual(ColorTokens.gray900.hex, "#121214")
+    }
 }
